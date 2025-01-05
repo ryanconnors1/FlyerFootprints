9f00d52a25cc36f83e7c0f71d1880cb2c8e4d595
Ryan Connors <ryannumber3@gmail.com>
2025-01-05 15:53:03 -0500

    Added App.js useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import InternshipTable from './components/InternshipTable';
